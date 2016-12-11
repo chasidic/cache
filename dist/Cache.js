cd3be7961406cd82565e8f920cbacb453c65bef7
Ilan Frumer <ilanfrumer@gmail.com>
2016-12-11 14:35:30 +0200

    tslib

--- dist/Cache.js
+++ dist/Cache.js
@@ -1,12 +1,5 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments)).next());
-    });
-};
+const tslib_1 = require("tslib");
 const url_1 = require("url");
 const path_1 = require("path");
 const fs_extra_promise_1 = require("fs-extra-promise");
@@ -26,31 +19,31 @@ class Cache {
         return path_1.resolve(this.CACHE_DIR, normalized);
     }
     clear() {
-        return __awaiter(this, void 0, void 0, function* () {
+        return tslib_1.__awaiter(this, void 0, void 0, function* () {
             yield fs_extra_promise_1.removeAsync(this.CACHE_DIR);
         });
     }
     has(key) {
-        return __awaiter(this, void 0, void 0, function* () {
+        return tslib_1.__awaiter(this, void 0, void 0, function* () {
             let filepath = this._normalize(key);
             return yield fs_extra_promise_1.existsAsync(filepath);
         });
     }
     remove(key) {
-        return __awaiter(this, void 0, void 0, function* () {
+        return tslib_1.__awaiter(this, void 0, void 0, function* () {
             let filepath = this._normalize(key);
             yield fs_extra_promise_1.removeAsync(filepath);
         });
     }
     get(key) {
-        return __awaiter(this, void 0, void 0, function* () {
+        return tslib_1.__awaiter(this, void 0, void 0, function* () {
             let filepath = this._normalize(key);
             let doesExists = yield fs_extra_promise_1.existsAsync(filepath);
             return doesExists ? yield fs_extra_promise_1.readFileAsync(filepath, { encoding: 'UTF-8' }) : null;
         });
     }
     set(key, value) {
-        return __awaiter(this, void 0, void 0, function* () {
+        return tslib_1.__awaiter(this, void 0, void 0, function* () {
             let filepath = this._normalize(key);
             let dirpath = path_1.dirname(filepath);
             yield fs_extra_promise_1.ensureDirAsync(dirpath);
