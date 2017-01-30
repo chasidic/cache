4496c500c1116a7589d365d4808803994b766933
Ilan Frumer <ilanfrumer@gmail.com>
2017-01-30 16:06:09 +0200

    remove null

--- src/ICache.ts
+++ src/ICache.ts
@@ -2,8 +2,8 @@ export interface ICache {
     clear(): Promise<void>;
     has(key: string): Promise<boolean>;
     remove(key: string): Promise<void>;
-    get(key: string): Promise<string | null>;
+    get(key: string): Promise<string>;
     set(key: string, value: string): Promise<void>;
-    getJSON<T>(key: string): Promise<T | null>;
+    getJSON<T>(key: string): Promise<T>;
     setJSON<T>(key: string, value: T): Promise<void>;
 }
