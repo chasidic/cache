49556381a709d162722db4eceb9f1eb2e4cd2751
Ilan Frumer <ilanfrumer@gmail.com>
2017-04-19 15:25:58 +0300

    versions

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,5 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 var MemoryCache_1 = require("./MemoryCache");
 exports.MemoryCache = MemoryCache_1.MemoryCache;
 var Cache_1 = require("./Cache");
