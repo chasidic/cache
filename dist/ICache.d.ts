70c16a6fe9b3e4fbad2968e9e5ceb039337877ae
Ilan Frumer <ilanfrumer@gmail.com>
2017-07-10 18:28:14 +0300

    versions

--- dist/ICache.d.ts
+++
@@ -1,9 +0,0 @@
-export interface ICache {
-    clear(): Promise<void>;
-    has(key: string): Promise<boolean>;
-    remove(key: string): Promise<void>;
-    get(key: string): Promise<string>;
-    set(key: string, value: string): Promise<void>;
-    getJSON<T>(key: string): Promise<T>;
-    setJSON<T>(key: string, value: T): Promise<void>;
-}
